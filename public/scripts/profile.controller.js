bf94ab9739e8d6c6418cd5edfefcef856e7bec3f
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-11-03 09:09:34 -0500

    logout works

--- public/scripts/profile.controller.js
+++ public/scripts/profile.controller.js
@@ -5,11 +5,17 @@ function ProfileController($http, $location) {
   var ctrl = this;
   getUser($http, ctrl);
 
+ctrl.logout = function (){
   $http.get('/users/logout').then(function(response){
     console.log('response', response.data);
   }, function(error) {
-    console.log('error getting questions', error);
+    console.log('error getting user to logout', error);
+  }).then(function(){
+    $location.path('/login');
+  }, function(error) {
+    console.log('error logging out', error);
   });
+}
 }
 
 function getUser ($http, ctrl) {
