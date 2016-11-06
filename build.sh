a62f216e33d4632958ec69c3dd49b1befe97ddc7
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-11-06 17:56:04 -0600

    styling slogans page search bar

--- build.sh
+++ build.sh
@@ -9,3 +9,15 @@ cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/vendors;
 
 cp node_modules/angular-route/angular-route.min.js public/vendors;
 cp node_modules/angular-route/angular-route.min.js.map public/vendors;
+
+
+
+cp node_modules/angular-bootstrap-calendar/dist/js/angular-bootstrap-calendar-tpls.min.js public/vendors;
+cp node_modules/angular-bootstrap-calendar/dist/js/angular-bootstrap-calendar-tpls.min.js.map public/vendors;
+
+cp node_modules/angular-bootstrap-calendar/dist/css/angular-bootstrap-calendar.min.css public/vendors;
+cp node_modules/angular-bootstrap-calendar/dist/css/angular-bootstrap-calendar.min.css.map public/vendors;
+
+cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js public/vendors;
+
+cp node_modules/moment/min/moment.min.js public/vendors;
--- public/scripts/app.module.js
+++ public/scripts/app.module.js
@@ -1,1 +1,1 @@
-angular.module('lojongApp', ['ngRoute', 'mwl.calendar', 'ui.bootstrap', 'ngAnimate', 'colorpicker.module']);
+angular.module('lojongApp', ['ngRoute', 'mwl.calendar', 'ui.bootstrap']);
