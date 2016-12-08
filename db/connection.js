056682ba831765540a00e87d5b57aa444450c015
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-12-08 14:04:43 -0600

    update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,28 +1,26 @@
-// const pg = require('pg');
-//
+const pg = require('pg');
+
 // var config = {
 //   database : 'slogans'
 // };
-//
-// var pool = new pg.Pool(config);
-//
-// module.exports = pool;
 
 const url = require('url')
 
 const params = url.parse(process.env.DATABASE_URL);
 const auth = params.auth.split(':');
 
-module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
+var config = {
+
       user: auth[0],
       password: auth[1],
       host: params.hostname,
       port: params.port,
       database: params.pathname.split('/')[1],
       ssl: true
-    }
-  }
+
+
 };
+
+var pool = new pg.Pool(config);
+
+module.exports = pool;
