ccfff6f738343b987e64f4381b5929d5844364b0
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-12-08 10:59:20 -0600

    change connection.js for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,28 @@
-const pg = require('pg');
+// const pg = require('pg');
+//
+// var config = {
+//   database : 'slogans'
+// };
+//
+// var pool = new pg.Pool(config);
+//
+// module.exports = pool;
 
-var config = {
-  database : 'slogans'
+const url = require('url')
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true
+    }
+  }
 };
-
-var pool = new pg.Pool(config);
-
-module.exports = pool;
